907565d958fd8fd0b761f5472934c68874477e94
Luke Thomas <lukethethomas@gmail.com>
2016-02-28 21:56:54 -0800

    buttons working, switching to desktop

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 $(document).ready(function() {
-	var boxbuttons = document.querySelectorAll(".box-button");
+	var introboxes = document.querySelectorAll(".intro-box");
 
-	for(var index = 0; index < boxbuttons.length; index++) {
-		$(boxbuttons[index]).hover(function() {
+	for(var index = 0; index < introboxes.length; index++) {
+		$(introboxes[index]).hover(function() {
 			$(this).toggleClass("button-hover");
 			}, function() {
 				$(this).toggleClass("button-hover");
 		});
 	}
+
+	// Skills button
+	$(introboxes[0]).click(function() {
+		$('body').animate({
+            scrollTop: $("#skills-container").offset().top
+        }, 100);
+	})
+
+	// Bio button
+	$(introboxes[1]).click(function() {
+		$('body').animate({
+            scrollTop: $("#bio-container").offset().top
+        }, 200);
+	})
+
+	// Projects button
+	$(introboxes[2]).click(function() {
+		$('body').animate({
+            scrollTop: $("#projects-container").offset().top
+        }, 300);
+	})
 });
